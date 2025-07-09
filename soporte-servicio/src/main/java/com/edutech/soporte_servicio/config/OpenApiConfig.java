b2ea5f82f46d42b262f9a1d6231cd584bb3255dc
Ignacio Jara <ign.jarap@duocuc.cl>
2025-07-08 22:35:11 -0400

    feat: Configuración de Swagger para OpenAPI

---
+++ soporte-servicio/src/main/java/com/edutech/soporte_servicio/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.edutech.soporte_servicio.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // Indicates that this class provides configurations for the Spring context
+public class OpenApiConfig {
+
+    @Bean // Defines a Spring bean for OpenAPI configuration
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de Soporte - EduTech") // API documentation title
+                        .version("1.0") // API version
+                        .description("Documentación de la API del microservicio de gestión de tickets de soporte para la plataforma EduTech.")); // API description
+    }
+}
